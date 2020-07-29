d6903ee96b32e8c149202f9c58f2ddb84982bbbe
ihsan akif <ihsan.akif@gmail.com>
2020-07-29 12:51:23 +0800

    Updated

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_with_firebase/services/auth.dart';
+import 'package:flutter_with_firebase/shared/constants.dart';
+import 'package:flutter_with_firebase/shared/loading.dart';
 
 class SignIn extends StatefulWidget {
   final Function toggleView;
@@ -11,14 +13,17 @@ class SignIn extends StatefulWidget {
 
 class _SignInState extends State<SignIn> {
   final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
+  bool loading = false;
 
   // text field state
   String email = '';
   String password = '';
+  String error = '';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return loading ? Loading() : Scaffold(
       backgroundColor: Colors.brown[100],
       appBar: AppBar(
         backgroundColor: Colors.brown[400],
@@ -36,12 +41,16 @@ class _SignInState extends State<SignIn> {
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
         child: Form(
+          key: _formKey,
           child: Column(
             children: <Widget>[
               SizedBox(
                 height: 20.0,
               ),
               TextFormField(
+                decoration: textInputDecoration.copyWith(hintText: 'Email'),
+                validator: (val) =>
+                    val.isEmpty ? 'Enter an email' : null, // null is valid/true
                 onChanged: (val) {
                   setState(() => email = val);
                 },
@@ -50,6 +59,10 @@ class _SignInState extends State<SignIn> {
                 height: 20.0,
               ),
               TextFormField(
+                decoration: textInputDecoration.copyWith(hintText: 'Password'),
+                validator: (val) => val.length < 6
+                    ? 'Enter a password 6+ chars long'
+                    : null, // null is valid/true
                 obscureText: true,
                 onChanged: (val) {
                   setState(() => password = val);
@@ -65,9 +78,26 @@ class _SignInState extends State<SignIn> {
                     style: TextStyle(color: Colors.white),
                   ),
                   onPressed: () async {
-                    print(email);
-                    print(password);
-                  })
+                    // evaluate true or false
+                    if (_formKey.currentState.validate()) {
+                      setState(() => loading = true);
+                      dynamic result = await _auth.signInWithEmailAndPassword(
+                          email, password);
+                      if (result == null) {
+                        setState(() {
+                          error = 'Could not sign in with those credentials';
+                          loading = false;
+                        });
+                      }
+                    }
+                  }),
+              SizedBox(
+                height: 12.0,
+              ),
+              Text(
+                error,
+                style: TextStyle(color: Colors.red, fontSize: 14.0),
+              )
             ],
           ),
         ),
