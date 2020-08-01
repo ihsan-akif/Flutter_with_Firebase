573bf5ca3508a542155c4bab1a348798f27ec860
ihsan akif <ihsan.akif@gmail.com>
2020-08-01 10:03:28 +0800

    Last commit

--- lib/screens/home/brew_list.dart
+++ lib/screens/home/brew_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_with_firebase/models/brew.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_with_firebase/screens/home/brew_tile.dart';
 
 class BrewList extends StatefulWidget {
   @override
@@ -10,15 +11,13 @@ class BrewList extends StatefulWidget {
 class _BrewListState extends State<BrewList> {
   @override
   Widget build(BuildContext context) {
-    
-    final brews = Provider.of<QuerySnapshot>(context);
-    //print(brews.documents);
-    for (var doc in brews.documents) {
-      print(doc.data);
-    }
+    final brews = Provider.of<List<Brew>>(context) ?? [];
 
-    return Container(
-      
+    return ListView.builder(
+      itemCount: brews.length,
+      itemBuilder: (context, index) {
+        return BrewTile(brew: brews[index]);
+      },
     );
   }
 }
--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,9 +1,9 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_with_firebase/models/brew.dart';
+import 'package:flutter_with_firebase/screens/home/settings_form.dart';
 import 'package:flutter_with_firebase/services/auth.dart';
 import 'package:flutter_with_firebase/services/database.dart';
 import 'package:provider/provider.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_with_firebase/screens/home/brew_list.dart';
 
 class Home extends StatelessWidget {
@@ -11,8 +11,20 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamProvider<QuerySnapshot>.value(
+    void _showSettingsPanel() {
+      showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return Container(
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
+              child: SettingsForm(),
+            );
+          });
+    }
+
+    return StreamProvider<List<Brew>>.value(
       value: DatabaseService().brews,
+      initialData: List(),
       child: Scaffold(
         backgroundColor: Colors.brown[50],
         appBar: AppBar(
@@ -25,10 +37,19 @@ class Home extends StatelessWidget {
                   await _auth.signOut();
                 },
                 icon: Icon(Icons.person),
-                label: Text('Logout'))
+                label: Text('Logout')),
+            FlatButton.icon(
+                onPressed: () => _showSettingsPanel(),
+                icon: Icon(Icons.settings),
+                label: Text('Settings'))
           ],
         ),
-        body: BrewList(),
+        body: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/coffee_bg.png'),
+                    fit: BoxFit.cover)),
+            child: BrewList()),
       ),
     );
   }
